479fb1751f5fb6bb26d7306bc059a87d9b9a9c48
Antal Bako <antal.bako@tradelink.co>
2023-11-02 21:50:57 +0100

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Lusitana } from 'next/font/google';
+import { Inter } from 'next/font/google';
+import {any} from 'prop-types'
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400','700']});
+export const inter = Inter({ subsets: ['latin'], weight: ['400','700']});
